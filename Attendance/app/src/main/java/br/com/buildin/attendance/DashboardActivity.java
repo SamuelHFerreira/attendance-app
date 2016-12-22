5503a2f439f6b7f5836d016139e4f362c5f6f0f0
samuel.ferreira <elsamuca2000@yahoo.com.br>
2016-12-21 23:57:52 -0200

    basics of project

---
+++ Attendance/app/src/main/java/br/com/buildin/attendance/DashboardActivity.java
@@ -0,0 +1,31 @@
+package br.com.buildin.attendance;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // TODO if max user reached
+                Snackbar.make(view, "Número máximo de usuários atingido", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
